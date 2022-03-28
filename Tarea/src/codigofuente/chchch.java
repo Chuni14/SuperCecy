5145dd63766fe0cb439dd72a8d22c007162dcb11
Alumno <Alumno@ED-CC2-12>
2022-03-28 10:15:36 -0700

    vALE

--- Tarea/src/codigofuente/chchch.java
+++ Tarea/src/codigofuente/chchch.java
@@ -5,7 +5,7 @@ public class chchch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");		
-		System.out.println("Hola Mare");
+		System.out.println("Hola Mare, estas bien jodida XDXDXD");
 		System.out.println("Hola Merellín");
 
 	}
